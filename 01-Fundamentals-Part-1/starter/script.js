b56bd4e04c65c687d7fb37d75b83e9f36116d19f
Oleg Mechytashvili <olegdrb@gmail.com>
2025-01-30 10:17:52 +0200

    feat: values and variables lesson completed

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,19 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is fun!');
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Oleh";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $fuunction = 27;
+let PI = 3.1514;
+
+let myFirstJob = "Teacher";
+let myCurrentJob = "Student";
+
+let job1 = "teacher";
+let job2 = "student";
+
+console.log(myFirstJob);
